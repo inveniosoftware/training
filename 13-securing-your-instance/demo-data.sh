af40f6ab7aea24383a6c694e3f4e370e41d38cba
Diego Rodriguez <diego.rodriguez@cern.ch>
2019-03-18 19:08:57 +0100

    13 use pipenv run for my-site commands

--- 13-securing-your-instance/demo-data.sh
+++ 13-securing-your-instance/demo-data.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 # Create users
-my-site users create admin@test.ch -a --password=123456 # create admin user ID 1
-my-site users create manager@test.ch -a --password=123456 # create admin user ID 2
-my-site users create visitor@test.ch -a --password=123456 # create visitor user ID 3
+pipenv run my-site users create admin@test.ch -a --password=123456 # create admin user ID 1
+pipenv run my-site users create manager@test.ch -a --password=123456 # create admin user ID 2
+pipenv run my-site users create visitor@test.ch -a --password=123456 # create visitor user ID 3
 
 # Create an admin token to create records
-ADMIN_ACCESS_TOKEN=`my-site tokens create -n bootcamptest -u admin@test.ch`
+ADMIN_ACCESS_TOKEN=`pipenv run my-site tokens create -n bootcamptest -u admin@test.ch`
 
 # Record owned by admin
 curl -k --header "Content-Type: application/json" --request POST --data '{"title":"My test record", "contributors": [{"name": "Doe, John"}], "owner": 1}' https://localhost:5000/api/records/?prettyprint=1 -H "Authorization: Bearer $ADMIN_ACCESS_TOKEN"
