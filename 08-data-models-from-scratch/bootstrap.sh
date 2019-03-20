2869b1b2a3dffdb7869c5fe1ac8de5135fb736d7
Zacharias Zacharodimos <zacharias.zacharodimos@cern.ch>
2019-03-20 10:52:17 +0100

    tutorial 08: fix bootstrap.sh

--- 08-data-models-from-scratch/bootstrap.sh
+++ 08-data-models-from-scratch/bootstrap.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-cp -R  ~/src/training/08-data-models-from-scratch/author_module/ ~/src/my-site/
+cp -R  ~/src/training/08-data-models-from-scratch/author_module/* ~/src/my-site/
