c86c38693e15be9ca513a86a37017c361a5a87f0
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2019-03-20 10:33:24 +0100

    global: delete version.py

--- 08-data-models-from-scratch/author_module/my_site/authors/__init__.py
+++ 08-data-models-from-scratch/author_module/my_site/authors/__init__.py
@@ -11,6 +11,5 @@
 from __future__ import absolute_import, print_function
 
 from .ext import Authors
-from .version import __version__
 
-__all__ = ('__version__', 'Authors')
+__all__ = ('Authors', )
